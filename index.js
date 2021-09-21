304d23ed6aff3b44ea9b4ae3bce58d073626be81
Jesse Brown <Brownjesse8895@yahoo.com>
2021-09-20 19:58:20 -0500

    commit tree 87b386540f2dc0d9c7d390b03b04878ef8cbdc21

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase()
 
-const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
+//const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`
 //const shortGreeting = `Welcome, ${currentUser[0]}!`
-// Line 8 is the way to get shortGreetingto pass with bracket notation 
+
